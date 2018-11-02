a2d192f426cdb0d60670a0d9f94a632a9a07f071
Changcheng Liu <changcheng.liu@intel.com>
2018-11-02 09:42:09 +0800

    fix version to be 13.2.2
    It's better to use tag to get this info
        git describe --match "v*" | sed 's/^v//'
    please tag the version
    
    Signed-off-by: Changcheng Liu <changcheng.liu@intel.com>
    Signed-off-by: Dehao Shang <dehao.shang@intel.com>

--- make-srpm.sh
+++ make-srpm.sh
@@ -3,9 +3,7 @@
 #
 # Create a SRPM which can be used to build Ceph
 #
-# ./make-srpm.sh <version>
-# rpmbuild --rebuild /tmp/ceph/ceph-<version>-0.el7.centos.src.rpm
-#
 
-./make-dist $1
+vers=`echo 'stx-ceph_v13.2.2' | sed 's/^stx-ceph_v//'`
+./make-dist $ver
 rpmbuild -D"_sourcedir `pwd`" -D"_specdir `pwd`" -D"_srcrpmdir `pwd`" -bs ceph.spec
