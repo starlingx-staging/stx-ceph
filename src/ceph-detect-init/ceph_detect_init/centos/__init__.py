54277dfb9d84e348440e767d62dc6ede78df0edd
Delfino Curado <delfinogomes.curadofilho@windriver.com>
2021-08-24 00:07:55 -0300

    Reverting ceph-disk removal
    Adding ceph-disk to continue to use the same version of puppet-ceph
    
    Signed-off-by: Delfino Curado <delfinogomes.curadofilho@windriver.com>

---
+++ src/ceph-detect-init/ceph_detect_init/centos/__init__.py
@@ -0,0 +1,12 @@
+distro = None
+release = None
+codename = None
+
+
+def choose_init():
+    """Select a init system
+
+    Returns the name of a init system (upstart, sysvinit ...).
+    """
+    # Don't use systemd
+    return 'sysvinit'
